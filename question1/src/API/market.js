cf40a776351a29181541317f07072c5729134367
Floyd To <69040807+floydto@users.noreply.github.com>
2021-10-10 19:15:08 +0800

    Update market.js

--- question1/src/API/market.js
+++ question1/src/API/market.js
@@ -1,8 +1,20 @@
-import axios from "axios"
+// import axios from "axios"
 
+// export const getSpotData = async () => {
+//     const res = await axios.get("https://ftx.com/api/markets");
+//     if (res.status === 200) {
+//         return res.data.result;
+//     }
+// }
+
+
+// use fetch
 export const getSpotData = async () => {
-    const res = await axios.get("https://ftx.com/api/markets");
-    if (res.status === 200) {
-        return res.data.result;
+    const marketData = await fetch(`https://ftx.com/api/markets`, {});
+    if (marketData.status === 200) {
+        return marketData.json().then(
+            data => {
+                return data.result
+            })
     }
 }
